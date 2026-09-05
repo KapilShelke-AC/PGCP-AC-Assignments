ae8d0ab89c10cb00a328373a8aa9d8cd66652de9
KapilShelke-AC <kapil.shelke.cmaug26@gmail.com>
2026-09-05 14:47:48 +0530

    Add files via upload

---
+++ Assignment_2/Assignment_2_que_1.1/src/Assignment_2_que_1.2/src/Assignment_2_que_1.2.cpp
@@ -0,0 +1,71 @@
+//============================================================================
+// Name        : 2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Patient{
+	int patientId;
+	string name;
+	int age;
+	string ward;
+	const string bloodGroup;
+
+public:
+      Patient():patientId(0),name("Unknown"),age(0),ward("General"),bloodGroup("O+"){
+    	  cout<<"[Constructor] Default patient registered"<<endl;
+      }
+      	  Patient(int id, const string& name):patientId(id),name(name),age(0),ward("Emergency"),bloodGroup("O+"){
+      		  cout<<"[Constructor] Emergency:"<<name<<endl;
+      	  }
+
+      	  Patient(int id, const string& name, int age, const string& ward, const string& bg)
+      	  :patientId(id),name(name),age(age),ward(ward),bloodGroup(bg){
+      		  cout<<"[Constructor] Full Admission "<<name<<endl;
+
+      	  }
+      	  ~Patient(){
+      		  cout<<"[Destructor] Patient "<<name<<"Discharged"<<endl;
+      	  }
+
+      	  void displayRecord() const{
+      		  cout<<"Patient Record :"<<endl;
+               cout<<"ID"<<"  :"<<patientId<<endl;
+               cout<<"Name"<<"  :"<<name<<endl;
+               cout<<"Age"<<"  :"<<age<<endl;
+               cout<<"Ward"<<"  :"<<ward<<endl;
+               cout<<"Blood Grp"<<"  :"<<bloodGroup<<endl;
+      	  }
+
+      	  void transferWard(const string& newWard){
+                 cout<<"Ward Transfer:"<<name<<" ->"<< newWard<<endl;
+                 ward = newWard;
+      	  }
+};
+
+int main() {
+
+    Patient p;
+
+    Patient p2(1001,"Kapil Shelke");
+
+    Patient p3(2001, "Ujjwal Shelke", 25, "Cardiology", "O+");
+
+    Patient* patients = new Patient[4];
+
+    /*for(int i=0;i<4;i++){
+    	patients[i].displayRecord();
+    }*/
+    p3.displayRecord();
+
+    p2.transferWard("ICU");
+
+      delete[] patients;
+
+      return 0;
+}

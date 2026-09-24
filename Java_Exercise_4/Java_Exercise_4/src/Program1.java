71ab1a3cddbbe201e002df28f7b7b8536054c0af
KapilShelke-AC <kapil.shelke.cmaug26@gmail.com>
2026-09-24 10:30:18 +0530

    Add files via upload

---
+++ Java_Exercise_4/Java_Exercise_4/src/Program1.java
@@ -0,0 +1,19 @@
+
+
+
+public class Program1 {
+
+	public static void main(String[] args) {
+	  StringBuffer s = new StringBuffer("Java");
+	  s.append(" ");
+	  s.append("Exercises");
+	  
+	  System.out.println(s.charAt(1));
+	  System.out.println(s.charAt(7));
+	  
+	  
+	  
+
+	}
+
+}
---
+++ Java_Exercise_4/Java_Exercise_4/src/Program2.java
@@ -0,0 +1,19 @@
+
+public class Program2 {
+
+	public static void main(String[] args) {
+	    String s1 = "Hello Java";
+	    String s2 = "Hello Java";
+	    String s3 = new String("Hello Java");
+	    
+	    if(s1 == s2) {
+	    	System.out.println("Strings are equals");
+	    }
+	    if(s1 != s3) {
+	    	System.out.println("This are not equal");
+	    }
+	    
+
+	}
+
+}
